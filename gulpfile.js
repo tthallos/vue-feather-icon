cf25063358cac45f6f8187a198760ddd50815f07
mage3k <loper.tt@gmail.com>
2017-06-09 23:54:33 +0800

    flatten components folder and support Vue plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,46 @@
+const path = require('path')
 const gulp = require('gulp')
 const es = require('event-stream')
 const rename = require('gulp-rename')
+const concat = require('gulp-concat')
+const flatten = require('gulp-flatten')
+const gutil = require('gulp-util')
 
-const wrap = function(header, footer) {
+const wrap = function(inject) {
   return es.map(function (file, cb) {
     const contents = String(file.contents)
+    const filename = path.basename(file.path, '.svg')
     try {
-      file.contents = new Buffer(`${header}${contents}${footer}`)
+      file.contents = new Buffer(inject(filename, contents))
     } catch (err) {
-      return cb(new gutil.PluginError('gulp-inject-string', err))
+      return cb(new gutil.PluginError('void', err))
     }
     cb(null, file)
   })
 }
 
-const header = '<template>\n'
-const footer = '</template>'
 
-gulp.task('default', () => {
+gulp.task('components', () => {
   gulp.src('feather/icons/**/*.svg')
-    .pipe(wrap(header, footer))
+    .pipe(wrap((name, content) => {
+      let c = content.replace(/\n/g, '')
+      return `  Vue.component('feather-${name}', '<template>${c}</template>')`
+    }))
+    .pipe(concat('index.js'))
+    .pipe(wrap((filename, content) => `exports.install = function(Vue) {\n${content}\n}`))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('component', () => {
+  gulp.src('feather/icons/**/*.svg')
+    .pipe(wrap((name, content) => {
+      return `<template>\n${content}</template>`
+    }))
     .pipe(rename({
       extname: '.vue'
     }))
-    .pipe(gulp.dest('components'))
+    .pipe(flatten())
+    .pipe(gulp.dest('./components'))
 })
+
+gulp.task('default', ['components', 'component'])
