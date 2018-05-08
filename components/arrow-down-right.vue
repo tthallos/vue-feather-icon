fa132bd783fc7d0b532cc3e009ce04354d2eb80d
Brandon Liles <brandon.liles@gmail.com>
2018-05-08 15:55:56 -0400

    Add Typescript support

--- components/arrow-down-right.vue
+++ components/arrow-down-right.vue
@@ -1,6 +1,16 @@
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <line x1="6" y1="6" x2="18" y2="18" fill="none" stroke="#000" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"/>
-    <polyline points="9 18 18 18 18 9" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <line x1="7" y1="7" x2="17" y2="17" />
+  <polyline points="17 7 17 17 7 17" />
 </svg>
 </template>
--- gulpfile.js
+++ gulpfile.js
@@ -35,13 +35,23 @@ gulp.task('pia', () => {
     .pipe(gulp.dest('./'))
 })
 
+gulp.task('typescript', () => {
+  gulp.src('feather/icons/**/*.svg')
+    .pipe(wrap((name, content) => {
+      return `  export class ${upCamelize(name)} {}`
+    }))
+    .pipe(concat('index.d.ts'))
+    .pipe(wrap((filename, content) => `import Vue from 'vue';\n\ndeclare module 'vue-feather-icon' {\n  export function install(): void;\n${content}\n}`))
+    .pipe(gulp.dest('./'))
+})
+
 gulp.task('components', () => {
   gulp.src('feather/icons/**/*.svg')
     .pipe(wrap((name, content) => {
-      return `  Vue.component('feather-${name}', require('./components/${name}.vue'))`
+      return `  Vue.component('feather-${name}', require('./components/${name}.vue').default)`
     }))
     .pipe(concat('plugin.js'))
-    .pipe(wrap((filename, content) => `module.exports = function(Vue) {\n  Vue.component('feather-icon', require('./components/index.vue'))\n${content}\n}`))
+    .pipe(wrap((filename, content) => `module.exports = function(Vue) {\n  Vue.component('feather-icon', require('./components/index.vue').default)\n${content}\n}`))
     .pipe(gulp.dest('./'))
 })
 
@@ -57,4 +67,4 @@ gulp.task('component', () => {
     .pipe(gulp.dest('./components'))
 })
 
-gulp.task('default', ['component', 'components', 'pia'])
+gulp.task('default', ['component', 'components', 'pia', 'typescript'])
